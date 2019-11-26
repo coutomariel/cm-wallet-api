a58c6cfea1b19ec137c7cd0333409bc71280b214
Mariel <coutomariel@gmail.com>
2019-11-26 20:30:02 -0300

    Configuração inicial para testes

--- src/test/java/com/coutomariel/wallet/WalletApiApplicationTests.java
+++ src/test/java/com/coutomariel/wallet/WalletApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.coutomariel.wallet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WalletApiApplicationTests {
 
 	@Test
