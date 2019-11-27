cd308135b142506aa6eb13664db2ce0a43389697
Mariel <coutomariel@gmail.com>
2019-11-27 11:06:51 -0300

    Criado service do User

--- src/main/java/com/coutomariel/wallet/repository/UserRepository.java
+++ src/main/java/com/coutomariel/wallet/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.coutomariel.wallet.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.coutomariel.wallet.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	Optional<User> findByEmailEquals(String email);
